013719104e077786c662b34935aedddff53dc8ad
abbymay1300 <122413276+abbymay1300@users.noreply.github.com>
2024-02-15 11:40:36 -0500

    adding comment from me :D

--- 3BlinkCode.ino
+++ 3BlinkCode.ino
@@ -40,6 +40,7 @@ void setup() {
       digitalWrite(A5, LOW);   // turn the LED off by making the voltage LOW
       delay(1000);                      // wait for a second
 
+      //COMMENT BY ABBY <3
       digitalWrite(A4, HIGH);  // turn the LED on (HIGH is the voltage level)
       delay(1000);                      // wait for a second
       digitalWrite(A4, LOW);   // turn the LED off by making the voltage LOW
